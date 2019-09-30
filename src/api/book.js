dd2930a6c5673b50e55a0175c3a6756a61f38311
沈如祥 <1638824607@qq.com>
2019-09-30 09:50:03 +0800

    first commit

---
+++ src/api/book.js
@@ -0,0 +1,129 @@
+import axios from 'axios'
+import { setLocalForage } from '@/utils/localForage'
+import { getCategoryName } from '@/utils/book'
+
+export function home2() {
+  return axios({
+    method: 'get',
+    url: `${process.env.VUE_APP_BOOK_URL}/book/home2`
+  })
+}
+
+export function detail(book) {
+  // return {
+  //   "error_code": 0,
+  //   "msg": "获取成功",
+  //   "data": {
+  //     "id": 228,
+  //     "fileName": "2017_Book_DigitalKenya",
+  //     "cover": "http://47.99.166.157/book/res/img//Economics/2017_Book_DigitalKenya.jpeg",
+  //     "title": "Digital Kenya",
+  //     "author": "Bitange Ndemo and Tim Weiss",
+  //     "publisher": "Palgrave Macmillan UK, London",
+  //     "bookId": "2017_Book_DigitalKenya",
+  //     "category": 3,
+  //     "categoryText": "Economics",
+  //     "language": "en",
+  //     "rootFile": "OEBPS/content.opf",
+  //     "selected": false,
+  //     "private": false,
+  //     "cache": false,
+  //     "haveRead": 0
+  //   }
+  // }
+
+  return axios({
+    method: 'get',
+    url: `${process.env.VUE_APP_BOOK_URL}/book/detail`,
+    params: {
+      fileName: book.fileName
+    }
+  })
+}
+
+export function flatList() {
+  return axios({
+    method: 'get',
+    url: `${process.env.VUE_APP_BOOK_URL}/book/flat-list`
+  })
+}
+
+export function list() {
+  return axios({
+    method: 'get',
+    url: `${process.env.VUE_APP_BASE_URL}/book/list`
+  })
+}
+
+export function home() {
+  return axios({
+    method: 'get',
+    url: `${process.env.VUE_APP_BASE_URL}/book/home`
+  })
+}
+
+export function shelf() {
+  return axios({
+    method: 'get',
+    url: `${process.env.VUE_APP_BASE_URL}/book/shelf`
+  })
+}
+
+export function downloadMp3(url, cb, cb2) {
+  axios.create({
+    baseURL: url,
+    method: 'get',
+    responseType: 'blob',
+    timeout: 30 * 1000,
+    onDownloadProgress: progressEvent => {
+      console.log(progressEvent)
+    }
+  }).get().then(response => {
+    const blob = new Blob([response.data])
+    if (cb) cb(blob)
+  }).catch(err => {
+    if (cb2) cb2(err)
+  })
+}
+
+export function download(item, onSuccess, onFailed, onError, onProgress) {
+  axios.create({
+    baseURL: process.env.VUE_APP_EPUB_URL,
+    method: 'get',
+    responseType: 'blob',
+    timeout: 180 * 1000,
+    onDownloadProgress: progressEvent => {
+      if (onProgress) onProgress(progressEvent)
+    }
+  }).get(`${getCategoryName(item.category)}/${item.fileName}.epub`)
+    .then(res => {
+      const blob = new Blob([res.data])
+      setLocalForage(item.fileName, blob, () => {
+        if (onSuccess) onSuccess(item)
+      }, err => {
+        if (onFailed) onFailed(err)
+      })
+    })
+    .catch(err => {
+      if (onError) onError(err)
+    })
+}
+
+export function downloadWithoutCache(item, cb, cb2, cb3) {
+  axios.create({
+    baseURL: process.env.VUE_APP_EPUB_URL,
+    method: 'get',
+    responseType: 'blob',
+    timeout: 30 * 1000,
+    onDownloadProgress: progressEvent => {
+      if (cb3) cb3(progressEvent)
+    }
+  }).get(`${getCategoryName(item.category)}/${item.fileName}.epub`)
+    .then(res => {
+      const blob = new Blob([res.data])
+      if (cb) cb(blob)
+    })
+    .catch(err => {
+      if (cb2) cb2(err)
+    })
+}
